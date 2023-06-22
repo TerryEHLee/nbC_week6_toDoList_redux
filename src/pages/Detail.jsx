923cc6d1b0ebc7dc59374a4df00ea8582251046d
TerryEHLee <terryehlee@gmail.com>
2023-06-23 06:29:47 +0900

    not work...

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Detail() {
-  return <div>Detail</div>;
+  const navigate = useNavigate();
+  const paramId = useParams().id;
+
+  const todos = useSelector((state) => state.todos);
+  const filteredTodo = todos.filter((item) => item.id === paramId)[0];
+
+  return (
+    <StyledDetailBox>
+      <h3 styled={{ marginBottom: "10px" }}>Detail</h3>
+      title: {filteredTodo.title} <br />
+      contents: {filteredTodo.contents} <br />
+      isDone: {filteredTodo.isDone.toString()} <br />
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        prev
+      </button>
+    </StyledDetailBox>
+  );
 }
 
 export default Detail;
+
+const StyledDetailBox = styled.div`
+  background-color: lavender
+  padding: 20px
+`;
